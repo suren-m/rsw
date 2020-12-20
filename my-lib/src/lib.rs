31c1f2512dd0f6d3a0f4b09ad92a9023111b9668
suren <suren>
2020-12-20 20:53:40 +0000

    created add function

--- my-lib/src/lib.rs
+++ my-lib/src/lib.rs
@@ -1,7 +1,13 @@
+
+
+pub fn add(a:i32, b:i32) -> i32 { a + b }
+
 #[cfg(test)]
 mod tests {
+    use crate::add;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(add(2,2), 4);
     }
 }
