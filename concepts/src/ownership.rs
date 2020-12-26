bafa9a1e69170456624c42e2b42c181edd1bdd10
suren <suren-m>
2020-12-26 21:11:01 +0000

    updated ownership with references

--- concepts/src/ownership.rs
+++ concepts/src/ownership.rs
@@ -1,7 +1,22 @@
 fn demo() {
-    let x = "hello";
-    let y = x;
-    println!("{}", x);
+    let x = String::from("hello");
+    let y = x; // ownership moved to y
+               // println!("{}", x); // compile time error
+
+    let s1 = String::from("hello");
+
+    // this is a bit too much
+    let (s1, len) = get_len(s1);
+    fn get_len(s: String) -> (String, usize) {
+        let len = s.len();
+        (s, len)
+    }
+
+    let len = get_len_v2(&s1);
+    dbg!(len);
+    fn get_len_v2(s: &String) -> usize {
+        s.len()
+    }
 }
 
 #[cfg(test)]
