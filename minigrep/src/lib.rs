2ddf3b7b939cdb3d166b4a46214e05d033ce3f50
suren <suren-m>
2021-01-03 18:29:40 +0000

    added essential feature for searching in minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -17,10 +17,37 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text\n:{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(()) // return unit type value
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
